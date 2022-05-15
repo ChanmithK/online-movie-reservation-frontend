c42c1a428eca29e3345f060e4d0bc4d221246514
ChanmithK <praveenkariyawasam0@gmail.com>
2022-05-15 12:52:21 +0530

    File structure modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Signin from "./containers/Signin";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./containers/Register";
+import LandingPage from "./containers/LandingPage";
+import StudentRegister from "./containers/StudentRegister";
 
 function App() {
   return (
-    //Comment
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/landing-page" element={<LandingPage />} />
+        <Route path="/student/signup" element={<StudentRegister />} />
+      </Routes>
     </div>
   );
 }
