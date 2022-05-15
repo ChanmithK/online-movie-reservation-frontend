7c105b04db842cfc764d72d8ee5c75182fcb3ad3
ChanmithK <praveenkariyawasam0@gmail.com>
2022-05-15 12:56:53 +0530

    file created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
 import "./App.css";
-import Signin from "./containers/Signin";
 import { Routes, Route } from "react-router-dom";
-import Signup from "./containers/Register";
-import LandingPage from "./containers/LandingPage";
-import StudentRegister from "./containers/StudentRegister";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/landing-page" element={<LandingPage />} />
-        <Route path="/student/signup" element={<StudentRegister />} />
-      </Routes>
+      <Routes></Routes>
     </div>
   );
 }
