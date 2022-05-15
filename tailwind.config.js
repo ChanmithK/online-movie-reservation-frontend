5229e035d1af9ed3f9527f8b4c965e08c22f2d5a
rasangalk <rasangalakshith38@gmail.com>
2022-05-15 13:40:59 +0530

    header designed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"main-orange": "#EC6B29",
+				"main-orangedrk": "#E85205",
+			},
+		},
 	},
 	plugins: [],
 };
