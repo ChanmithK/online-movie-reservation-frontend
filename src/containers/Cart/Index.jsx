e1e0e4da45ed38ef5c1cb8660f09ddeb8984767d
rasangalk <rasangalakshith38@gmail.com>
2022-05-17 15:33:44 +0530

    add to cart implemented

--- src/containers/Cart/Index.jsx
+++ src/containers/Cart/Index.jsx
@@ -2,66 +2,79 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Navigate } from "react-router-dom";
 import { getCart, RemoveFromCart } from "../../actions/movies.action";
+import Header from "../../components/Layouts/HeaderTwo";
+import "./style.css";
 
 function Cart() {
-  const cart = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getCart());
-  }, []);
+	const cart = useSelector(state => state.cart);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(getCart());
+	}, []);
 
-  const RemoveCartItem = (id) => {
-    // e.preventDefault();
-    const data = {
-      id: id,
-    };
-    console.log(data);
-    dispatch(RemoveFromCart(data));
-    window.location.reload(false);
-  };
+	const RemoveCartItem = id => {
+		// e.preventDefault();
+		const data = {
+			id: id,
+		};
+		console.log(data);
+		dispatch(RemoveFromCart(data));
+		window.location.reload(false);
+	};
 
-  return (
-    <table responsive="sm">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cart.cartItems.length > 0
-          ? cart.cartItems.map((cart) =>
-              cart.cartItems.map((test) => (
-                <tr
-                  //   onClick={() => showProductDetailsModal(product)}
-                  key={test._id}
-                  style={{ cursor: "Pointer" }}
-                >
-                  <td>{test.movie.movieName}</td>
-                  <td>{test.movie.price}</td>
-                  <td>
-                    <button
-                      style={{ background: "red" }}
-                      onClick={() => {
-                        RemoveCartItem(test._id);
-                      }}
-                    >
-                      Remove
-                    </button>
-                    <button style={{ background: "green" }}>
-                      <Link to={"/user/booking/" + test.movie._id}>
-                        Continue
-                      </Link>
-                    </button>
-                  </td>
-                </tr>
-              ))
-            )
-          : null}
-      </tbody>
-    </table>
-  );
+	return (
+		<div className='bg-scope-blue w-screen h-screen'>
+			<Header />
+			<h3 className='text-white font-medium text-4xl text-left mx-[10rem] mt-[5rem]'>
+				Cart
+			</h3>
+
+			<table className='ml-[10rem] mt-8'>
+				<div className='w-[70rem]'>
+					<thead>
+						<tr className='text-lg font-medium bg-white '>
+							<th className='w-[30rem] text-left'>Movie Name</th>
+							<th className='w-[15rem] text-left'>Price</th>
+							<th className='w-[20rem] text-center'>Action</th>
+						</tr>
+					</thead>
+				</div>
+
+				<div className='example h-64 overflow-y-scroll w-[66rem]'>
+					<tbody className=' bg-white'>
+						{cart.cartItems.length > 0
+							? cart.cartItems.map(cart =>
+									cart.cartItems.map(test => (
+										<tr className='text-lg  '>
+											<td className='w-[30rem] text-left'>
+												{test.movie.movieName}
+											</td>
+											<td className='w-[15rem] text-left'>
+												{test.movie.price}
+											</td>
+											<td className='w-[20rem] text-center flex gap-2 justify-center'>
+												<button
+													className='hover:bg-main-purpledrk bg-main-purple  px-3 rounded-xl text-white font-medium text-left'
+													onClick={() => {
+														RemoveCartItem(test._id);
+													}}>
+													Remove
+												</button>
+												<button className='hover:bg-main-orangedrk bg-main-orange  px-3 rounded-xl text-white font-medium text-left'>
+													<Link to={"/user/booking/" + test.movie._id}>
+														Continue
+													</Link>
+												</button>
+											</td>
+										</tr>
+									)),
+							  )
+							: null}
+					</tbody>
+				</div>
+			</table>
+		</div>
+	);
 }
 
 export default Cart;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 
-  theme: {
-    extend: {
-      colors: {
-        "main-orange": "#EC6B29",
+	theme: {
+		extend: {
+			colors: {
+				"main-orange": "#EC6B29",
 
-        "main-orangedrk": "#E85205",
+				"main-orangedrk": "#E85205",
 
-        "scope-blue": "#000025",
-      },
+				"scope-blue": "#000025",
 
-      width: {
-        48: "100rem",
-      },
-    },
-  },
+				"main-purple": "#36224E",
 
-  plugins: [],
+				"main-purpledrk": "#261936",
+			},
+
+			width: {
+				48: "100rem",
+			},
+		},
+	},
+
+	plugins: [],
 };
