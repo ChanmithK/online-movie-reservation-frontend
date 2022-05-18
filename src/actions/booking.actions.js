cddcdcead245bdf58173275ab4e7b6ec2390279e
rasangalk <rasangalakshith38@gmail.com>
2022-05-19 05:10:39 +0530

    ticket frontend implemented

--- src/actions/booking.actions.js
+++ src/actions/booking.actions.js
@@ -1,28 +1,46 @@
 import axios from "../helpers/axios";
 import { bookingConstants } from "./constants";
 
-export const AddBooking = (booking) => {
-  return async (dispatch) => {
-    dispatch({ type: bookingConstants.ADD_BOOKING_REQUEST });
-    const res = await axios.post("/user/booking", {
-      ...booking,
-    });
+export const AddBooking = booking => {
+	return async dispatch => {
+		dispatch({ type: bookingConstants.ADD_BOOKING_REQUEST });
+		const res = await axios.post("/user/booking", {
+			...booking,
+		});
 
-    if (res.status === 201) {
-      const { message } = res.data;
-      dispatch({
-        type: bookingConstants.ADD_BOOKING_SUCCESS,
-        payload: {
-          message,
-        },
-      });
-    } else {
-      if (res.status === 400) {
-        dispatch({
-          type: bookingConstants.ADD_BOOKING_FAILURE,
-          payload: { error: res.data.error },
-        });
-      }
-    }
-  };
+		if (res.status === 201) {
+			const { message } = res.data;
+			dispatch({
+				type: bookingConstants.ADD_BOOKING_SUCCESS,
+				payload: {
+					message,
+				},
+			});
+		} else {
+			if (res.status === 400) {
+				dispatch({
+					type: bookingConstants.ADD_BOOKING_FAILURE,
+					payload: { error: res.data.error },
+				});
+			}
+		}
+	};
+};
+
+export const getBooking = id => {
+	return async dispatch => {
+		dispatch({ type: bookingConstants.GET_BOOKING_REQUEST });
+		const res = await axios.get(id);
+		if (res.status === 201) {
+			dispatch({
+				type: bookingConstants.GET_BOOKING_SUCCESS,
+				payload: { book: res.data.movie },
+			});
+		} else {
+			dispatch({
+				type: bookingConstants.GET_BOOKING_FAILURE,
+				payload: { error: res.data.error },
+			});
+		}
+	};
 };
--- src/components/Layouts/PDF.jsx
+++ src/components/Layouts/PDF.jsx
@@ -1,8 +1,47 @@
-import React, { useRef } from "react";
-import jsPDF from "jspdf";
+import React, { useState, useEffect } from "react";
+import Header from "../../components/Layouts/HeaderBlack";
+import QRCode from "react-qr-code";
+import ReactDOM from "react-dom";
 
 function PDF() {
-	return <div>PDF</div>;
+	const [movie, setMovie] = useState("The Batman");
+	const [date, setdate] = useState("2022.07.08");
+	const [time, setTime] = useState("11:30");
+	const [seats, setSeats] = useState(2);
+
+	return (
+		<div>
+			<Header />
+			<h3 className='text-black font-bold text-4xl text-left mx-[10rem] mt-[2rem]'>
+				Ticket
+			</h3>
+			<div className='grid grid-cols-3 mx-[10rem] justify-items-start mt-24'>
+				<div className=''>
+					<div className='text-left'>
+						<h2 className='font-regular text-lg'>Movie Name</h2>
+						<p className='font-bold text-3xl'>The Batman</p>
+					</div>
+					<div className='text-left mt-12'>
+						<h2 className='font-regular text-lg'>Time</h2>
+						<p className='font-bold text-3xl'>11:30</p>
+					</div>
+				</div>
+				<div className=''>
+					<div className='text-left'>
+						<h2 className='font-regular text-lg'>Date</h2>
+						<p className='font-bold text-3xl'>01/05/2022</p>
+					</div>
+					<div className='text-left'>
+						<h2 className='font-regular text-lg mt-12'>Seat Count</h2>
+						<p className='font-bold text-3xl'>02</p>
+					</div>
+				</div>
+				<div className='ml-[3rem]'>
+					<QRCode value='6281026b3f17d985c159835a' />,
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default PDF;
