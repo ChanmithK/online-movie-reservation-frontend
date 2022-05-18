c9cada892617881092b81df738204f6a8e78fe4d
rasangalk <rasangalakshith38@gmail.com>
2022-05-19 03:40:29 +0530

    reservations page implemented without proper css

--- src/components/Layouts/RightLayout.jsx
+++ src/components/Layouts/RightLayout.jsx
@@ -3,14 +3,17 @@ import LayoutCover from "../../images/LayoutCover.png";
 
 function RightLayout() {
 	return (
-		<div className='bg-slate-300 h-full relative'>
-			<h1 className='absolute w-full text-center bottom-48 font-bold text-xl py-10 text-white'>
+		<div className='bg-slate-300 h-screen relative'>
+			<h1 className='absolute w-full text-center top-[2rem] font-bold text-5xl py-10 text-white mx-auto'>
 				VOX CINEMAZ
 			</h1>
-			<p className='absolute w-full font-medium text-center text-white px-10 bottom-28 text-left'>
+			{/* <p className='absolute w-full font-medium text-center text-white px-10 top-[10rem] text-left text-lg mx-auto'>
 				Did you know that you can book out a cinema hall at VOX Cinemas?​
-			</p>
-			<img src={LayoutCover} className='bg-sky-500 h-full object-cover' />
+			</p> */}
+			<img
+				src={LayoutCover}
+				className='bg-sky-500 h-full w-full object-cover'
+			/>
 		</div>
 	);
 }
